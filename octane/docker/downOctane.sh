5eed41b6583b346d51e1f18d1ecf49b2615e5ac8
panama69 <github@flynnshome.com>
2016-10-22 20:03:32 -0500

    Update downOctane.sh

--- octane/docker/downOctane.sh
+++ octane/docker/downOctane.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-docker stop octane octane_es octane_ora
-docker rm -v octane octane_es octane_ora
+docker stop octane octane_es octane_oracle
+docker rm -v octane octane_es octane_oracle
 docker network rm octane_nw
 
 sudo rm -rf /usr/lib/oracle/xe /var/elasticsearch/data /opt/octane
