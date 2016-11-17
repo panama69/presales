4d620da434b8b0309c80657b036fdbb777ffb55c
Dave Flynn <github@flynnshome.com>
2016-11-17 10:30:55 -0600

    switched to be more POSIX compliant.  removed 'function' declaration and switched test from '[[ ]]' to '[ ]'

--- octane/docker/select-menu.sh
+++ octane/docker/select-menu.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 #export TERM=vt100
+#
+# Author: Dave Flynn <flynn@hpe.com>
+# 
 
 demofile=$1
-if [[ $# -ne 1 ]]
+if [ $# -ne 1 ]
 then
    echo Missing the json demo configuration file.
    echo This is the file which determines the arguments needed for the
@@ -14,9 +17,9 @@ fi
 # you will need to install dialog to use this script.
 # on Ubuntu, use 'sudo apt-get install dialog'
 
-function have_network_connection ()
+have_network_connection ()
 {
-     if [[ `ping www.flynnshome.com -c 1|grep -w "0% packet loss"|wc -l` -lt 1 ]]
+     if [ `ping www.flynnshome.com -c 1|grep -w "0% packet loss"|wc -l` -lt 1 ]
      then
           dialog --title "WARNING" --msgbox "Unable to successfully ping www.flynnshome.com.  Check your network connection" 10 70
           exit -1
@@ -24,10 +27,11 @@ function have_network_connection ()
           return 1
      fi
 }
+
 # args:
 #    1 - container_name
 #    2 - search_string
-function wait_on ()
+wait_on ()
 {
      #echo wait_on ">$1<" ">$2<"
      # wait for process to be up by checking the logs
@@ -37,12 +41,12 @@ function wait_on ()
      echo -ne $progressStr'\r'
      cmd="$1 | grep "$2" | wc -l"
      echo $cmd
-     while [[ $(eval $cmd) -lt 1 ]]
+     while [ $(eval $cmd) -lt 1 ]
      do
         sleep 10
         progressStr=$progressStr"#"
         echo -ne $progressStr'\r'
-        if [[ $counter -le 10 ]]
+        if [ $counter -le 10 ]
         then
            (( counter++ ))
         else
@@ -51,7 +55,7 @@ function wait_on ()
         fi
      done
 
-     if [[ $counter -lt 1 ]]
+     if [ $counter -lt 1 ]
      then
           return -1
      else
@@ -60,7 +64,7 @@ function wait_on ()
 }
 
 
-function create_networks ()
+create_networks ()
 {
    netJson=$1
    networksCnt=`echo $netJson |jq ".|length"`
@@ -69,17 +73,17 @@ function create_networks ()
       networkName=`echo $netJson |jq ".[$i]"`
       networkName=${networkName//\"}
       echo $networkName
-      if [[ -z "$networkName" ]] || [[ "$networkName" = "null" ]]
+      if [ -z "$networkName" ] || [ "$networkName" = "null" ]
       then
          echo WARNING: Can not create a network of null value
       else
          # if the network doesn't exist already then create it else use the existing one
          cmd="docker network ls | grep $networkName | wc -l"
-         if [[ $(eval $cmd) -eq 0 ]]
+         if [ $(eval $cmd) -eq 0 ]
          then
             docker network create "$networkName" > /dev/null
             # quick check to see if the network was created or exists
-            if [[ $(eval $cmd) -ne 1 ]]
+            if [ $(eval $cmd) -ne 1 ]
             then
                echo WARNING -- network doesn\'t exist
             else
@@ -92,7 +96,7 @@ function create_networks ()
    done
 }
 
-function build_docker_string ()
+build_docker_string ()
 {
    foundContainerName=false
    foundImage=false
@@ -117,7 +121,7 @@ function build_docker_string ()
             ;;
          Ports)
             count=`echo $value|jq ".|length"`
-            if [[ "$count" -gt 0 ]]
+            if [ "$count" -gt 0 ]
             then
                s=''
                for ((x=0; x<$count; x++))
@@ -130,7 +134,7 @@ function build_docker_string ()
             ;;
          Networks)
             count=`echo $value|jq ".|length"`
-            if [[ "$count" -gt 0 ]]
+            if [ "$count" -gt 0 ]
             then
                s=''
                for ((x=0; x<$count; x++))
@@ -143,7 +147,7 @@ function build_docker_string ()
             ;;
          DataContainers)
             count=`echo $value|jq ".|length"`
-            if [[ "$count" -gt 0 ]]
+            if [ "$count" -gt 0 ]
             then
                s=''
                for ((x=0; x<$count; x++))
@@ -156,7 +160,7 @@ function build_docker_string ()
             ;;
          EnvironmentVariables)
             count=`echo $value|jq ".|length"`
-            if [[ "$count" -gt 0 ]]
+            if [ "$count" -gt 0 ]
             then
                s=''
                for ((x=0; x<$count; x++))
@@ -169,7 +173,7 @@ function build_docker_string ()
             ;;
          Volumes)
             count=`echo $value|jq ".|length"`
-            if [[ "$count" -gt 0 ]]
+            if [ "$count" -gt 0 ]
             then
                s=''
                for ((x=0; x<$count; x++))
@@ -193,7 +197,7 @@ function build_docker_string ()
 
 # args:
 #    1 - Json string object containing configurations
-function start_Postgres ()
+start_Postgres ()
 {
    echo "###"
    echo "### Example of command need to validate before operational"
@@ -206,7 +210,7 @@ function start_Postgres ()
 
 # args:
 #    1 - Json string object containing configurations
-function start_MobileCenter ()
+start_MobileCenter ()
 {
    echo "###"
    echo "### Example of command need to validate before operational"
@@ -220,7 +224,7 @@ function start_MobileCenter ()
 
 # args:
 #    1 - Json string object containing configurations
-function start_Jenkins ()
+start_Jenkins ()
 {
    docker_str=$(build_docker_string "$1")
    cmd="docker run -d --restart=always $docker_str"
@@ -230,7 +234,7 @@ function start_Jenkins ()
 
 # args:
 #    1 - Json string object containing configurations
-function start_OracleXE ()
+start_OracleXE ()
 {
    docker_str=$(build_docker_string "$1")
    cmd="docker run -d --shm-size=2g --restart=always $docker_str"
@@ -247,7 +251,7 @@ function start_OracleXE ()
   
    cmd="docker logs $tmp"
    wait_on "$cmd" "$searchmsg"
-   if [[ $? -eq 0 ]]
+   if [ $? -eq 0 ]
    then
       echo Database is ready
    else
@@ -258,7 +262,7 @@ function start_OracleXE ()
 
 # args:
 #    1 - Json string object containing configurations
-function start_ElasticSearch()
+start_ElasticSearch()
 {
    docker_str=$(build_docker_string "$1")
    cmd="docker run -d --shm-size=2g --restart=always $docker_str"
@@ -270,7 +274,7 @@ function start_ElasticSearch()
 
 # args:
 #    1 - Json string object containing configurations
-function start_Octane ()
+start_Octane ()
 {
    docker_str=$(build_docker_string "$1")
 
@@ -296,7 +300,7 @@ function start_Octane ()
    do
       i=${i//\"} # remove all quotes.  assumpiton is a quoted string
       # assumption is wrapper.log is found in the octane log folder
-      if [[ ${i##*/} = "log" ]]
+      if [ ${i##*/} = "log" ]
       then
          logPath=${i%:*}
       fi
@@ -307,7 +311,7 @@ function start_Octane ()
    echo "   -> sleeping for 30 sec before checking"
    sleep 30
    wait_on "$cmd" "$searchstr"
-   if [[ $? -eq 0 ]]
+   if [ $? -eq 0 ]
    then
       tail -2 $logPath/wrapper.log
    else
@@ -323,7 +327,7 @@ function start_Octane ()
 
 # args
 #   $1 - demo menu choice
-function container_list ()
+container_list ()
 {
    containers=`echo $demosJson |jq ".[$1].Dependencies.Systems[].Assets[].Configs |select (.ContainerName) |.ContainerName"`
    echo "$containers"
@@ -331,7 +335,7 @@ function container_list ()
 
 # args
 #   $1 - demo menu choice
-function stop_containers ()
+stop_containers ()
 {
    containerList=$1
    echo Stopping containers $containerList
@@ -341,7 +345,7 @@ function stop_containers ()
 
 # args
 #   $1 - demo menu choice
-function start_containers ()
+start_containers ()
 {
    echo Need to add a check to see if the the container exists in the stopped state
    containerList=$1
@@ -352,14 +356,14 @@ function start_containers ()
 
 # args
 #   $1 - demo menu choice
-function remove_containers ()
+remove_containers ()
 {
    containerList=$1
    #create a list of folder mounts to be removed after container removed
    for i in $containerList
    do 
       i=${i//\"}
-      if [[ $i != "jenkins" ]] && [[ $i != "jenkins-dc" ]]
+      if [ $i != "jenkins" ] && [ $i != "jenkins-dc" ]
       then
          folderList=("${folderList[@]}" "`docker inspect $i |jq '.[].Mounts[].Source'`")
       fi
@@ -379,12 +383,12 @@ function remove_containers ()
 
 # args
 #   $1 - demo menu choice
-function show_container_warning ()
+show_container_warning ()
 {
-   containers=$(container_list $1)
+   local containers=$(container_list $1)
    dialog --title "WARNING" --defaultno --yesno "One of these containers exist:\n     $containers\nYou must take care of them before performing this operation.\n\n         DO YOU WANT TO STOP AND REMOVE THEM" 20 70
 
-   if [[ $? -eq 0 ]]
+   if [ $? -eq 0 ]
    then
       echo Removing...
       stop_containers "$containers"
@@ -397,9 +401,9 @@ function show_container_warning ()
 # Check to see if the container exists in docker
 # args
 #   $1 - demo menu choice
-function containers_exist ()
+containers_exist ()
 {
-   containers=$(container_list "$1")
+   local containers=$(container_list "$1")
 
    IFS=$'\n'
 
@@ -407,14 +411,14 @@ function containers_exist ()
    for i in $containers
    do
      i=${i//\"}
-     cmd="docker ps -a --format='{{.Names}}' |grep -w "$i" |wc -l"
+     local cmd="docker ps -a --format='{{.Names}}' |grep -w "$i" |wc -l"
      let count=($count)+$(eval $cmd)
-     echo $i
+     #echo $i
    done
    return $count
 }
 
-function download_files ()
+download_files ()
 {
        tmpJson="$1"
        restoreLocation=`echo $tmpJson |jq ".Path"`
@@ -426,7 +430,7 @@ function download_files ()
        file=${file//\"}
 
        echo "Processing: $file"
-       if [[ -e "/opt/$file" ]]
+       if [ -e "/opt/$file" ]
        then
             echo "$file" exists already locally so no need to download
        else
@@ -444,11 +448,11 @@ function download_files ()
 
 
 # will return an array (list) based on the json query passed
-function get_list ()
+get_demo_list ()
 {
    IFS=$'\n'
-   index=0 #index of the array element
-   menuItem=0 #menu item number
+   local index=0 #index of the array element
+   local menuItem=0 #menu item number
    for i in `jq "$1" $demofile`
    do  
       options[index]=$menuItem
@@ -470,7 +474,7 @@ function get_list ()
 # other resource www.linuxcommand.org/lc3_adv_dialog.php 
 # other http://jrgraphix.net/man/D/dialog
 dialog --title "sudo" --yesno "This utility requires 'sudo'\n\nIf you didn't run this as:\n     sudo ./select-menu.sh\n\nPlease exit and restart.\n\nDo you wish to continue?" 20 40
-if [[ $? -ne 0 ]]
+if [ $? -ne 0 ]
 then
      exit -1
 fi
@@ -481,12 +485,11 @@ fi
 cmd=(dialog --backtitle "Demo Menu System" --menu "Select options:" 19 76 12 )
 demoCnt=`jq ".Demos|length" $demofile`
 demosJson=`jq ".Demos" $demofile`
-get_list ".Demos[].Name"
+get_demo_list ".Demos[].Name"
 
 choice=$("${cmd[@]}" "${options[@]}" 2>&1 >/dev/tty)
 #if select menu choice > than available demos then exit
-echo flynn $choice -- $demoCnt
-if [[ $choice -eq $demoCnt ]] || [[ -z $choice ]]
+if [ $choice -eq $demoCnt ] || [ -z $choice ]
 then
    clear
    echo "Goodbye"
@@ -494,7 +497,7 @@ then
 fi
 
 containers_exist "$choice"
-if [[ $? -ne 0 ]]
+if [ $? -ne 0 ]
 then
    show_container_warning "$choice"
 fi
@@ -519,7 +522,7 @@ do
       assetName=`echo $assetsJson |jq ".[$assetsIndex].Name"`
       assetName=${assetName//\"}
       dataJson=`echo $assetsJson |jq ".[$assetsIndex].Data"`
-      if [[ `echo $dataJson |jq ".|length"` -eq 2 ]]
+      if [ `echo $dataJson |jq ".|length"` -eq 2 ]
       then
          download_files "$dataJson"
       fi
